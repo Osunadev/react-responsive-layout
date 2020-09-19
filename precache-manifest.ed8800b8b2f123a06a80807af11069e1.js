3921b073602bd8f623104f2ce26f8047023e1261
Omar Osuna <omar.alonso.osuna.angulo@uabc.edu.mx>
2020-09-19 13:19:21 -0700

    Updates

---
+++ precache-manifest.ed8800b8b2f123a06a80807af11069e1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aed98d0e7f842b93ab779fdf5034205b",
+    "url": "/react-responsive-layout/index.html"
+  },
+  {
+    "revision": "fdc927a77195c4644e73",
+    "url": "/react-responsive-layout/static/css/main.b93262e3.chunk.css"
+  },
+  {
+    "revision": "9c690fd983f758bf5f0e",
+    "url": "/react-responsive-layout/static/js/2.18ff290b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-responsive-layout/static/js/2.18ff290b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fdc927a77195c4644e73",
+    "url": "/react-responsive-layout/static/js/main.dafa17de.chunk.js"
+  },
+  {
+    "revision": "352d7ac255a22fb22e44",
+    "url": "/react-responsive-layout/static/js/runtime-main.47eecb80.js"
+  },
+  {
+    "revision": "cc168914e67dbd7bf2426b7fe64f0a33",
+    "url": "/react-responsive-layout/static/media/Blinky Empty Cart-01.cc168914.png"
+  }
+]);
